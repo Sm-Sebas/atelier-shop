4c6b243cf06c48d7633207f30e01acdb92c3a544
Sebastopol3 <sanchezmarinez11@gmail.com>
2026-02-03 18:12:20 +0100

    Fix case sensitivity and paths

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Link from 'next/link';
 import { useEffect } from 'react';
-import { useCart } from '../context/CartContext';
+import { useCart } from '@/context/CartContext';
 
 export default function SuccessPage() {
   const { clearCart } = useCart();
